79d0239a2d51cbf192ee5286aa6b2295d7dffe38
Ameliyah <liyah.cims@gmail.com>
2025-06-17 17:48:40 +0700

    form pencarian

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-// Toggle class active
+// Toggle class active untuk hamburger menu
 const navbarnav = document.querySelector(".navbar-nav");
 // Ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarnav.classList.toggle("active");
 };
 
-// Klik di luar sidebar untuk menghilang nav
-const hamburger = document.querySelector("#hamburger-menu");
+// toggle class active untuk search form
+const searchform = document.querySelector(".search-form");
+const searchbox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchform.classList.toggle("active");
+  searchbox.focus();
+  e.preventDefault();
+};
+
+// Klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarnav.contains(e.target)) {
     navbarnav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchform.contains(e.target)) {
+    searchform.classList.remove("active");
   }
 });
