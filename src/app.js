e6530c5216cc4a830ed248b225994e63aaf2a420
Ameliyah <liyah.cims@gmail.com>
2025-06-25 15:11:04 +0700

    Menambah kan Icon Star dan Tampilan Harga

--- src/app.js
+++ src/app.js
@@ -9,3 +9,12 @@ document.addEventListener("alpine:init", () => {
     ],
   }));
 });
+
+// Konversi Ke Rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
